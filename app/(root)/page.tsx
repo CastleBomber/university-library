4450fa6ad896c6b5d5b2c187c39a800218f71781
cBombs <richie.ucsb@gmail.com>
2025-06-12 16:55:10 -0700

    moving project

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,16 @@
 import BookList from "@/components/ui/BookList";
 import BookOverview from "@/components/ui/BookOverview";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { sampleBooks } from "@/constants";
 
 const Home = () => {
   return (
     <>
-      <BookOverview />
-      <BookList />
+      <BookOverview {...sampleBooks[0]}/>
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
     </>
   );
 };
---
+++ constants/tmp.ts
@@ -0,0 +1,72 @@
+export const navigationLinks = [
+  {
+    href: "/library",
+    label: "Librar",
+  },
+  {
+    img: "/icons/user.svg",
+    selectedImg: "/icons/user-fill.svg",
+    href: "/my-profile",
+    label: "My Profile",
+  },
+];
+
+export const adminSideBarLinks = [
+  {
+    img: "/icons/admin/home.svg",
+    route: "/admin",
+    text: "Home",
+  },
+  {
+    img: "/icons/",
+    route: "/admin/",
+    text: "",
+  },
+  {
+    img: "/icons/",
+    route: "/admin/",
+    text: "",
+  },
+  {
+    img: "/icons/",
+    route: "/admin/",
+    text: "",
+  },
+  {
+    img: "/icons/",
+    route: "/admin/",
+    text: "",
+  },
+];
+
+export const FIELD_NAMES = {
+  fullName: "Full name",
+  email: "Email",
+  universityId: "University ID Number",
+  password: "Password",
+  universityCard: "Upload University ID card",
+};
+
+export const FIELD_TYPES = {
+  fullName: "text",
+  email: "email",
+  universityId: "number",
+  password: "password",
+};
+
+export const sampleBooks = [
+  {
+    id: 1,
+    title: "The Midnight Library",
+    authoer: "Matt Haig",
+    genre: "Fantasy / Fiction",
+    rating: 4.6,
+    total_copies: 20,
+    available_copies: 10,
+    description: "x",
+    color: "#1c1f40",
+    cover: "https://",
+    video: "/sammple-vidoe.mp4",
+    summary: "x",
+  },
+];
